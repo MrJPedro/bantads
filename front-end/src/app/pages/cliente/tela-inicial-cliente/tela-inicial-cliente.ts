eb15acdc1260f3ca982f0f08c0d764d6cb80a236
João Pedro Nossol <jpnossol@gmail.com>
2026-03-17 20:24:00 -0300

    Refactor TelaInicialCliente component to use standalone

--- front-end/src/app/pages/cliente/tela-inicial-cliente/tela-inicial-cliente.ts
+++ front-end/src/app/pages/cliente/tela-inicial-cliente/tela-inicial-cliente.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SaldoResponde } from '../../../DTO/conta/saldo-responde';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TextareaModule } from 'primeng/textarea';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ClienteParaAprovarResponse } from '../../../DTO/cliente/cliente-para-aprovar-response';
 
 @Component({
-  selector: 'app-tela-inicial-cliente',
-  imports: [],
-  templateUrl: './tela-inicial-cliente.html',
-  styleUrl: './tela-inicial-cliente.css',
+    selector: 'app-tela-inicial-cliente',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        CardModule,
+        ButtonModule,
+        DialogModule,
+        TextareaModule,
+        ToastModule
+    ],
+    providers: [MessageService],
+    templateUrl: './tela-inicial-cliente.html'
 })
 export class TelaInicialCliente {
 
+  conta = signal<SaldoResponde>({} as SaldoResponde);
+
+  cor = signal(this.conta().saldo >= 0.0 ? 'green' : 'red');
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+        this.mockDados();
+  }
+
+  mockDados() {
+        this.conta.set({ 
+          ...this.conta(), 
+          cliente: 'Ricardo Silva', 
+          conta: '1234', 
+          saldo: 1500.00 });
+
+        this.cor.set(this.conta().saldo >= 0.0 ? 'green' : 'red');
+    }
 
 }
