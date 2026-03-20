f91a85d3d735e546f293752ee866744b0dfdd291
Kauan Kelvin Alves Macedo <kauanmacedo85@outlook.com>
2026-03-20 17:20:43 +0000

    CORREÇÃO TEMA GLOBAL

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
