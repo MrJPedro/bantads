0b517e5d2370147f94a25638c338de707c465530
Kauan Macedo <kauanmacedo85@outlook.com>
2026-04-18 14:30:07 -0300

    backend gerente v1

---
+++ back-end/MS-Gerente/gerente-service/src/main/kotlin/com/bantads/gerente_service/gerente_model/Gerente.kt
@@ -0,0 +1,28 @@
+package com.bantads.gerente_service.gerente_model
+
+import jakarta.persistence.*
+import java.io.Serializable
+
+@Entity
+@Table(name = "tb_gerente")
+class Gerente : Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(nullable = false, unique = true, length = 11)
+    var cpf: String = ""
+
+    @Column(nullable = false)
+    var nome: String = ""
+
+    @Column(nullable = false, unique = true)
+    var email: String = ""
+
+    @Column(length = 20)
+    var telefone: String = ""
+
+    @Column(name = "qtd_clientes", nullable = false)
+    var quantidadeClientes: Int = 0
+}
