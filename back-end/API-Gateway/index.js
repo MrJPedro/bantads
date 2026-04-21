d0d632669e60966b83852a58ad7e19c4ca6feb1e
CaixoDarksixo <jpnossol@gmail.com>
2026-04-20 22:48:13 -0300

    API Gateway em NodeJS (Burrei antes)

---
+++ back-end/API-Gateway/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config(); // olhar o .env
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: process.env.FRONT, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['*'], 
+    credentials: true     
+}));
+
+app.use('/login', createProxyMiddleware({
+    target: process.env.AUTH_URI,
+    changeOrigin: true,
+}));
+
+app.use('/gerentes', createProxyMiddleware({
+    target: process.env.GERENTES_URI,
+    changeOrigin: true,
+}));
+
+app.use('/clientes', createProxyMiddleware({
+    target: process.env.CLIENTES_URI,
+    changeOrigin: true,
+}));
+
+app.use('/contas', createProxyMiddleware({
+    target: process.env.CONTAS_URI,
+    changeOrigin: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`API Gateway rondando na porta: http://localhost:${PORT}`);
+});
